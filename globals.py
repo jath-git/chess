5aeb7ade545ff010617a9fda0ec8b6fb5ef9a030
Jath <61468377+jath-git@users.noreply.github.com>
2022-12-31 23:17:53 -0500

    add colour codes

--- globals.py
+++ globals.py
@@ -15,4 +15,13 @@ POND_PROMOTION = 1
 POND_PASSING = 2
 CASTLE = 3
 
+bcolors = {
+    'BLUE': '\033[94m',
+    'PURPLE': '\033[95m',
+    'WHITE': '\033[0m',
+    'GREEN': '\x1b[6;30;42m',
+    'YELLOW': '\x1b[6;30;43m',
+    'DEFAULT': '\x1b[0m',
+}
+
 pieceSquareMap = {}
